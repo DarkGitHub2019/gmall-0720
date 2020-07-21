1cfa50dde41d73d9b40f6531d6dfcd7f4d069ca6
DarkGitHub2019 <1625293404@qq.com>
2020-07-21 08:52:46 +0800

    项目初始搭建完毕

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gmall-0720
 商城项目
+
+老师项目地址：https://github.com/joedyli/gmall-0211.git
---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/SpuDescMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.SpuDescEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * spu信息介绍
+ * 
+ * @author dark
+ * @email dark@163.com
+ * @date 2020-07-20 17:21:25
+ */
+@Mapper
+public interface SpuDescMapper extends BaseMapper<SpuDescEntity> {
+	
+}
